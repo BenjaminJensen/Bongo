3df9957d846e07511190bf0248c65bf874043418
Benjamin Jensen <ben@tamam.dk>
2020-03-21 19:45:13 +0100

    tmp

--- esp32/genvex_rotor/mqtt.py
+++ esp32/genvex_rotor/mqtt.py
@@ -9,4 +9,5 @@ client1= paho.Client("control1")                           #create client object
 
 client1.on_publish = on_publish                          #assign function to callback
 client1.connect(broker,port)                                 #establish connection
-ret= client1.publish("/topic/qos0","on")                   #publish
+#ret= client1.publish("homeassistant/binary_sensor/garden/config",'{"name": "garden", "device_class": "motion", "state_topic": "homeassistant/binary_sensor/garden/state"}')                   #publish
+ret= client1.publish("homeassistant/binary_sensor/garden/config",'')                   #publish
